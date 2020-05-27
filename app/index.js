5b329c046b98f47f49feabee5d667755eabf7873
victor <victorpsf2@hotmail.com>
2020-05-26 21:18:35 -0300

    correção da porta

--- app/index.js
+++ app/index.js
@@ -8,7 +8,7 @@ class App extends Api {
     async serverListen() {
         this.socketio(this.protocol);
         this.express.use(require('../controller'));
-        this.protocol.listen(this.procotolOption.port, () => this.log({
+        this.protocol.listen({ port: this.procotolOption.port }, () => this.log({
             message: `server open in ${this.protocolName}://${this.procotolOption.host}:${this.procotolOption.port}/`
         }));
     }
